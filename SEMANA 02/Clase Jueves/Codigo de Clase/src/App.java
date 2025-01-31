a631ca29a8fb7fa28fc704b7ff27e9e00902d110
Flisito <cris_c9@hotmail.com>
2025-01-30 18:33:22 -0600

    update

---
+++ SEMANA 02/Clase Jueves/Codigo de Clase/src/App.java
@@ -0,0 +1,8 @@
+//Proceso para crear un programa estructurado
+//
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+}
